acd52216fd8792193f70549816b08deded7e91d5
Luiz E <doginuwu@gmail.com>
2024-10-12 02:01:00 -0300

    feat: add map

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -13,11 +13,15 @@ map("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
 map("n", "<leader>bn", ":bnext<CR>", { noremap = true, silent = true })
 map("n", "<leader>bp", ":bprevious<CR>", { noremap = true, silent = true })
 map("n", "<leader>bd", ":bdelete<CR>", { noremap = true, silent = true })
+map("n", "<leader>bh", ":split<CR>", { noremap = true, silent = true })
+map("n", "<leader>bv", ":vsplit<CR>", { noremap = true, silent = true })
 
 local telescope = require("telescope.builtin")
 map("n", "<leader>ff", telescope.find_files, { desc = "Telescope find files" })
 map("n", "<leader>fg", telescope.live_grep, { desc = "Telescope live grep" })
-map("n", "<leader>fb", telescope.buffers, { desc = "Telescope buffers" })
+map("n", "<leader>b", telescope.buffers, { desc = "Telescope buffers" })
 map("n", "<leader>fh", telescope.help_tags, { desc = "Telescope help tags" })
+
+map("n", "<leader>fd", "<cmd> lua vim.diagnostic.open_float() <CR>", { desc = "Floating diagnostic" })
 
 -- Autocmd
